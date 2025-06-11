75a0318b046b235d759e5a8086eef6f9af02d5f8
Juan Segura <147679702+SebasthSegura@users.noreply.github.com>
2025-06-11 09:27:43 -0500

    create and uploading project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import com.company.sebasth.ject_banck.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int response;
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("************************************************");
+        System.out.println("*Bienvenido a la app bancaria estimado cliente!*");
+        System.out.println("************************************************");
+
+        System.out.println("Por favor, ingrese su nombre:");
+
+
+    }
+}
---
+++ src/models/Cashier.java
@@ -0,0 +1,4 @@
+package models;
+
+public class Cashier extends Employed {
+}
